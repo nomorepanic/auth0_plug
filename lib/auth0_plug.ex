8d6979e2abb0bb62a295f3713918a2e649881bc8
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-12-28 16:03:45 +0100

    feat/auth0plug: add call and init methods

--- lib/auth0_plug.ex
+++ lib/auth0_plug.ex
@@ -2,4 +2,12 @@ defmodule Auth0Plug do
   @moduledoc """
   Documentation for Auth0Plug.
   """
+
+  def init(options) do
+    options
+  end
+
+  def call(conn, _options) do
+    conn
+  end
 end
--- test/auth0_plug_test.exs
+++ test/auth0_plug_test.exs
@@ -1,4 +1,12 @@
 defmodule Auth0PlugTest do
   use ExUnit.Case
   doctest Auth0Plug
+
+  test "init/1" do
+    assert Auth0Plug.init(:options) == :options
+  end
+
+  test "call/2" do
+    assert Auth0Plug.call(:conn, :options) == :conn
+  end
 end
