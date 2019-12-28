ac1b59a5287e2395e7b8f3b8241429f2aad4be04
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-12-28 16:40:32 +0100

    feat/auth0plug: add get_jwt method

--- lib/auth0_plug.ex
+++ lib/auth0_plug.ex
@@ -2,9 +2,19 @@ defmodule Auth0Plug do
   @moduledoc """
   Documentation for Auth0Plug.
   """
+  alias Plug.Conn
 
   def init(options) do
     options
+  end
+
+  @doc """
+  Extracts the jwt from the header, when present.
+  """
+  def get_jwt(conn) do
+    conn
+    |> Conn.get_req_header("authorization")
+    |> List.first()
   end
 
   def call(conn, _options) do
--- test/auth0_plug_test.exs
+++ test/auth0_plug_test.exs
@@ -1,9 +1,23 @@
 defmodule Auth0PlugTest do
   use ExUnit.Case
+  alias Plug.Conn
+
   doctest Auth0Plug
 
   test "init/1" do
     assert Auth0Plug.init(:options) == :options
+  end
+
+  test "get_jwt/1" do
+    dummy Conn, [{"get_req_header", fn _a, _b -> [:header] end}] do
+      assert Auth0Plug.get_jwt(:conn) == :header
+    end
+  end
+
+  test "get_jwt/1 with an empty list" do
+    dummy Conn, [{"get_req_header", fn _a, _b -> [] end}] do
+      assert Auth0Plug.get_jwt(:conn) == nil
+    end
   end
 
   test "call/2" do
