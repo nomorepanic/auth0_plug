7477984e67f8fe6688b6c27e9e0413d1335bc064
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-12-28 22:29:10 +0100

    chore/mix: version bump to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Auth0Plug.MixProject do
   def project do
     [
       app: :auth0_plug,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
